6e6e06c93f28bb88b2411c081bf4fa0f8a3dc543
George Bockari <bitwhys@gmail.com>
2023-01-09 20:55:41 -0500

    chore: add deps, create tsconfig and eslint-config projects

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'turbo',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 0,
+  },
+}
